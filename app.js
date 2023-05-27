df2164353cf319d187acef646086575fe3675eb0
mateus202020 <mgsslayla@gmail.com>
2023-05-27 11:01:26 -0300

    desafio express

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+
+// Rota padrão
+app.get('/', (req, res) => {
+  const endpoint = '/';
+  const method = 'GET';
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}`);
+});
+
+// Rota /clientes
+app.get('/cliente', (req, res) => {
+  const endpoint = '/clientes';
+  const method = 'GET';
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}`);
+});
+app.post('/cliente', (req, res) => {
+  const endpoint = '/clientes';
+  const method = 'GET';
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}`);
+});
+app.put('/cliente', (req, res) => {
+  const endpoint = '/clientes';
+  const method = 'GET';
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}`);
+});
+app.get('/cliente', (req, res) => {
+  const endpoint = '/clientes';
+  const method = 'GET';
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}`);
+});
+
+// Rota /produtos
+app.get('/servico', (req, res) => {
+  const endpoint = '/clientes';
+  const method = 'GET';
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}`);
+});
+app.post('/servico', (req, res) => {
+  const endpoint = '/clientes';
+  const method = 'GET';
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}`);
+});
+app.put('/servico', (req, res) => {
+  const endpoint = '/clientes';
+  const method = 'GET';
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}`);
+});
+app.delete('/servico', (req, res) => {
+  const endpoint = '/clientes';
+  const method = 'GET';
+  res.send(`Endpoint: ${endpoint}\nMethod: ${method}`);
+});
+
+app.listen(8000, () => {
+  console.log('App ouvindo na porta 8000!');
+});
